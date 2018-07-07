a9cc7194362bdc0baa3a2d225dd5df8ce9a82b06
Will Cassella <wcassella@gmail.com>
2018-07-07 13:09:58 -0700

    Added clipboard stuff

--- WSL/setup.sh
+++ WSL/setup.sh
@@ -4,12 +4,13 @@ sudo apt upgrade -y
 sudo apt autoremove -y
 
 # Install packages
-sudo apt install -y make cmake gcc clang nasm neovim emscripten fish
+sudo apt install -y make cmake gcc clang nasm neovim emscripten fish mingw-w64
 
 # Setup neovim
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 mkdir -p ~/.config/nvim
 echo 'source ~/src/config/nvim/init.vim' > ~/.config/nvim/init.vim
+echo 'source ~/src/config/WSL/vim_clipboard.vim' >> ~/.config/nvim/init.vim
 nvim -c ':PlugUpdate' -c ':qa'
 
 # Make fish the default shell
---
+++ WSL/vim_clipboard.vim
@@ -0,0 +1,12 @@
+let g:clipboard = {
+\   'name': 'wsl_clipboard',
+\   'copy': {
+\       '+': 'clip.exe',
+\       '*': 'clip.exe',
+\   },
+\   'paste': {
+\       '+': 'pasterboy.exe',
+\       '*': 'pasterboy.exe',
+\   },
+\   'cache_enabled': 1,
+\}
