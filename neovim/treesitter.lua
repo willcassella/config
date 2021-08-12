b6ff516a8df0375d27b539c602c296ee6ff2525f
Will Cassella <cassew@google.com>
2021-08-12 12:06:41 -0700

    Cleanup

--- neovim/treesitter.lua
+++ neovim/treesitter.lua
@@ -1,5 +1,16 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "c", "cpp", "rust" },
+  ensure_installed = {
+      "c",
+      "cpp",
+      "rust",
+      "toml",
+      "json",
+      "lua",
+      "html",
+      "css",
+      "javascript",
+      "python",
+  },
   highlight = {
     enable = true,
   },
