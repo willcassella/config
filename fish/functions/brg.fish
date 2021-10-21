d2e2d5a8e39572b140207e30b2e7f474e7289c28
Will Cassella <will@willcassella.com>
2021-10-21 11:58:24 -0700

    Remove brg.fish

--- fish/functions/brg.fish
+++
@@ -1,9 +0,0 @@
-function brg
-    # Enumerate files where matches are found
-    for f in (rg -l $argv)
-        # Colorize those files with bat, and then re-search them
-        echo
-        echo $f':'
-        bat --plain --color always $f | rg --line-number --context 3 $argv
-    end
-end
