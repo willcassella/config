db193b916cd8c842d4a961ce805efc1f074d6a98
Will Cassella <will@willcassella.com>
2021-10-21 11:57:29 -0700

    Remove powershell script (not using anymore)

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++
@@ -1,7 +0,0 @@
-function prompt {
-    Write-Host ("" + $Env:username + "@" + $Env:computername) -nonewline -foregroundcolor DarkGray
-    Write-Host (":") -nonewline -foregroundcolor Gray
-    Write-Host ("" + $PWD) -nonewline -foregroundcolor DarkCyan
-    Write-Host ("$") -nonewline -foregroundcolor Gray
-    return " "
-}
