1247ab64395418241b5b2919b35585b5bb35cccd
Will Cassella <will@willcassella.com>
2025-11-17 08:16:23 -0800

    Update fish prompt

--- fish/config.fish
+++ fish/config.fish
@@ -15,57 +15,57 @@ set COLOR_HOST brgreen
 set COLOR_PWD brwhite
 set COLOR_ERROR ff3f3f
 set COLOR_GIT_BRANCH brwhite
-set COLOR_GIT_NOBRANCH $color_error
+set COLOR_GIT_NOBRANCH "$color_error"
 set COLOR_GIT_UNSTAGED bryellow
 set COLOR_GIT_STAGED brgreen
 
 function fish_prompt
     # Capture last status
-    set last_status $status
+    set last_status "$status"
 
     # Set host name/box name
-    set box_name (hostname)
+    set box_name "$(hostname)"
     if set -q CONTAINER_ID
         set box_name "$box_name($CONTAINER_ID)"
     end
 
     # Output a nice little box with username, host, and pwd
-    set_color $COLOR_BORDER; echo -n '[ '
-    set_color $COLOR_USER; echo -n (whoami)
-    set_color $COLOR_HOST; echo -n "@$box_name:"
-    set_color $COLOR_PWD; echo -n (prompt_pwd)
-    set_color $COLOR_BORDER; echo -n ' ]'
+    set_color "$COLOR_BORDER"; echo -n '[ '
+    set_color "$COLOR_USER"; echo -n "$(whoami)"
+    set_color "$COLOR_HOST"; echo -n "@$box_name:"
+    set_color "$COLOR_PWD"; echo -n "$(prompt_pwd)"
+    set_color "$COLOR_BORDER"; echo -n ' ]'
 
     # Get the current branch we're on, if we're in a git repo
-    if type -q git
-        and set -l git_branch (git rev-parse --abbrev-ref HEAD 2> /dev/null)
+    if type -q git; and git in-worktree
+        set -l git_branch "$(git rev-parse --abbrev-ref HEAD)"
         # Output git branch, or HEAD if detached
-        set_color $COLOR_BORDER; echo -n '['
+        set_color "$COLOR_BORDER"; echo -n '['
         if [ "$git_branch" = HEAD ]
-            set_color $COLOR_GIT_NOBRANCH; echo -n HEAD
+            set_color "$COLOR_GIT_NOBRANCH"; echo -n HEAD
         else
-            set_color $COLOR_GIT_BRANCH; echo -n $git_branch
+            set_color "$COLOR_GIT_BRANCH"; echo -n "$git_branch"
         end
 
         # Output number unstaged and staged changes
-        set -l unstaged_changes (git diff --name-only | uniq | wc -l | tr -d ' ')
-        set -l staged_changes (git diff --name-only --staged | wc -l | tr -d ' ')
+        set -l unstaged_changes "$(git diff --name-only | uniq | wc -l | tr -d ' ')"
+        set -l staged_changes "$(git diff --name-only --staged | wc -l | tr -d ' ')"
         if [ "$unstaged_changes" -ne 0 ]
-            set_color $COLOR_GIT_UNSTAGED; echo -n " ~$unstaged_changes"
+            set_color "$COLOR_GIT_UNSTAGED"; echo -n " ~$unstaged_changes"
         end
         if [ "$staged_changes" -ne 0 ]
-            set_color $COLOR_GIT_STAGED; echo -n " ✔$staged_changes"
+            set_color "$COLOR_GIT_STAGED"; echo -n " ✔$staged_changes"
         end
 
-        set_color $COLOR_BORDER; echo -n ']'
+        set_color "$COLOR_BORDER"; echo -n ']'
     end
 
     # Print last status if error
-    if test $last_status -ne 0
-        set_color $COLOR_ERROR; printf '[%d]' $last_status
+    if test "$last_status" -ne 0
+        set_color "$COLOR_ERROR"; printf '[%d]' "$last_status"
     end
 
     # Put command on a new line
-    set_color $COLOR_BORDER; printf '\n$ '
+    set_color "$COLOR_BORDER"; printf '\n$ '
     set_color normal
 end
